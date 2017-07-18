b5bd608fca9869ed6a0ece7f168fff0d2de14801
Arron Atchison <arron.atchison@gmail.com>
2017-07-18 17:21:07 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use iron::status;
 
 fn main() {
     fn hello_world(_: &mut Request)  -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "protobomb\nprimordial soups\nrecipe futures\n\nwritten_in_rust\n\nsoupRSpecialWBreadSticksWTFftw")))
+        Ok(Response::with((status::Ok, "protobomb\nprimordial soups\nrecipe futures\n\nwritten_in_rust")))
     }
 
     let _server = Iron::new(hello_world).http("0.0.0.0:5000").unwrap();
